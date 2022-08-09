060cfaf5ec98ee9832b706778bdeea5993897e6d
minossotam <tamyminosso@live.com>
2022-08-09 20:06:45 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -24,11 +26,43 @@ public class Main {
         mentoria.setData(LocalDate.now());
 
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
 
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+
+        Dev devTamy = new Dev();
+        devTamy.setNome("Tamy");
+        devTamy.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdo inscritos Tamy: " + devTamy.getConteudoInscritos());
+        devTamy.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdo inscritos Tamy: " + devTamy.getConteudoInscritos());
+        System.out.println("Conteúdo concluidos Tamy: " + devTamy.getConteudoConcluidos());
+        System.out.println("XP: " + devTamy.calcularTotalXp());
+
+
+        System.out.println("---------------");
+
+        Dev devZe = new Dev();
+        devZe.setNome("Zé");
+        devZe.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdo inscritos Zé: " + devZe.getConteudoInscritos());
+        devZe.progredir();
+        devZe.progredir();
+        devZe.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdo inscritos Zé: " + devZe.getConteudoInscritos());
+        System.out.println("Conteúdo concluidos Zé: " + devZe.getConteudoConcluidos());
+        System.out.println("XP: " + devZe.calcularTotalXp());
     }
 
 }
